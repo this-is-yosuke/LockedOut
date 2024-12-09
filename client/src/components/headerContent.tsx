3e638d11cffceb9dced1947975cec333131e9ea6
Kristy Thompson <kristy.h.thompson@gmail.com>
2024-12-09 12:35:33 -0700

    broker header into componenets

---
+++ client/src/components/headerContent.tsx
@@ -0,0 +1,24 @@
+import CTAButton from "./CTAButton";
+
+const HeaderContent = () => {
+  return (
+    <div className="relative z-10 flex items-center justify-center w-full h-full text-center text-white px-4 sm:px-6 lg:px-8">
+      <div>
+        {/* Title */}
+        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4">
+          Escape the Mystery
+        </h1>
+
+        {/* Subtitle */}
+        <p className="text-lg sm:text-xl md:text-2xl mb-6 max-w-2xl mx-auto">
+          Can you solve all four riddles before the clock runs out? You must answer correctly to unlock the final lock. Time is ticking!
+        </p>
+
+        {/* Call to Action Button */}
+        <CTAButton />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderContent;
---
+++ client/src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as FooterBottom } from './footerBottom';
+export { default as FooterSection } from './footerSection';
+export { default as SocialMediaIcons } from './socialMediaIcons';
+export { default as CTAButton } from './CTAbutton';
+export { default as HeaderContent } from './headerContent';
+export { default as HeaderOverlay } from './headerOverlay';
