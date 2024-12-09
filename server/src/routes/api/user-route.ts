f85f45edbc1149fddddcc42976f1f69051d75bd1
Dylan Middleton <dm.middleton92@gmail.com>
2024-12-09 15:09:32 -0700

    Installed 2 packages, npm i react-router-dom & jwt decode

---
+++ server/src/routes/api/user-route.ts
@@ -0,0 +1,9 @@
+import {Router} from 'express';
+// import type {Request, Response} from 'express';
+//import bcrypt from 'bcrypt';
+
+//import {User} from '../../models/user';
+
+const router = Router();
+
+//CREATE a new user
