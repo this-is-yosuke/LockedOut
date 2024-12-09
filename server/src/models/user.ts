aad1a3ccdf424ec7b857e71e8a7e91863cf82ea2
Dylan Middleton <dm.middleton92@gmail.com>
2024-12-09 15:43:09 -0700

    Updated user.ts & index.ts

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,1 +1,69 @@
-// Test
+import { DataTypes, type Sequelize, Model, type Optional } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+interface UserAttributes {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> { }
+
+export class User
+    extends Model<UserAttributes, UserCreationAttributes>
+    implements UserAttributes {
+    public id!: number;
+    public username!: string;
+    public email!: string;
+    public password!: string;
+
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+
+    // Hash the password before saving the user
+    public async setPassword(password: string) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(password, saltRounds);
+    }
+}
+
+export function UserFactory(sequelize: Sequelize): typeof User {
+    User.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true, //cannot duplicate names in the database
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: 'users',
+            sequelize,
+            hooks: {
+                beforeCreate: async (user: User) => {
+                    await user.setPassword(user.password);
+                },
+                beforeUpdate: async (user: User) => {
+                    await user.setPassword(user.password);
+                },
+            },
+        }
+    );
+
+    return User;
+}
