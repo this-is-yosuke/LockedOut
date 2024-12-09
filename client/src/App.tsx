7100638f9ff0e4371f0715b375db721b0c5086f9
Kristy Thompson <kristy.h.thompson@gmail.com>
2024-12-09 11:07:12 -0700

    adding in header section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
-import { Nav } from '../containers';
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home'; // Assuming Home is your home page component
 
 function App() {
-
   return (
-    <>
-      <Nav />
-    </>
-  )
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
