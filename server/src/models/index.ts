852f57156760466fd5e722514f02fb0036f6ff91
this-is-yosuke <yosuke.kibe@gmail.com>
2024-12-09 21:15:31 -0700

    Updated index.ts for models
    Imported some object factories

---
+++ server/src/models/index.ts
@@ -0,0 +1,7 @@
+// connection.ts needs to be finished
+import { Sequelize } from "../config/connection.ts";
+import { RoomFactory } from './room.js';
+import { RiddleFactory } from './room.js'
+import { UserFactory } from './user.js';
+import { AttemptFactor } './attempt.js';
+
