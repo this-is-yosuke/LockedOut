22c24a60914fa211822e6f477d59b0e4a9e4e827
this-is-yosuke <yosuke.kibe@gmail.com>
2024-12-11 21:39:03 -0700

    Updated attempt-seeds
    Added a comment

---
+++ server/src/seeds/attempt-seeds.ts
@@ -0,0 +1,6 @@
+
+/* Based on folder 20, it looks like intermediary tables neither have model data nor seed data. The mentioned files
+   might be deleted later. Dani confirmed that model and seed data are not necessary because of sequelize. HOWEVER,
+   if we wanted to add something a little extra to a table that just has a couple of IDs, then we'd need those files.
+   I will keep the attempt model for now, but the attempt seeds are not needed presently.*/
+
