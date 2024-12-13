338fd6eb86695221eeb245de85c4fcf0e3cce876
this-is-yosuke <yosuke.kibe@gmail.com>
2024-12-13 01:09:28 -0700

    Updated roomSeeds
    Changed the first two rooms' types from number to letter in order to match the riddle seeds

--- server/src/seeds/room-seeds.ts
+++ server/src/seeds/room-seeds.ts
@@ -7,8 +7,8 @@ export const seedRooms = async () => {
            a single question, usually utilizing a bad pun. */
         /* Type = number OR letter. Type was added with the lock type in mind; some combinations use letter, while
            others use numbers. */
-        {title: 'Ancient Crypt', description: 'Solve spooky riddles to escape this tomb.', type: 'number', difficulty: 3, creatorID: 1},
-        {title: 'Lost in Space', description: 'Complete these sci-fi riddles to escape the alien spacecraft!', type: 'number', difficulty: 2, creatorID: 2},
+        {title: 'Ancient Crypt', description: 'Solve spooky riddles to escape this tomb.', type: 'letter', difficulty: 3, creatorID: 1},
+        {title: 'Lost in Space', description: 'Complete these sci-fi riddles to escape the alien spacecraft!', type: 'letter', difficulty: 2, creatorID: 2},
         {title: 'The Endless Ikea', description: 'You suddenly find yourself in the infamous Endless Ikea. Solve the riddles and make it out alive!', type: 'letter', difficulty: 4, creatorID: 3}
     ])
 }
