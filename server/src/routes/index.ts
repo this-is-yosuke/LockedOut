d42ccf89afe9ad3961fe1d55115b382c827be834
Kristy Thompson <kristy.h.thompson@gmail.com>
2024-12-18 20:55:44 -0700

    loading rooms on homepage

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes.js';
 import apiRoutes from './api/index.js';
+import riddleRoutes from './api/index.js';
+
 //import { riddleToken } from '../middleware/auth.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/api', apiRoutes);
+router.use('/api', riddleRoutes);
+
 
 export default router;
