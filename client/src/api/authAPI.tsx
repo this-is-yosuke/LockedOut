2d036301d9164158f7d8ed22b2fee2a28fce2a91
Dylan Middleton <dm.middleton92@gmail.com>
2024-12-19 19:12:55 -0700

    Added new file added REGISTER to authAPI

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,5 +1,6 @@
 //add register and login API calls -dylan
 import type { UserLogin } from '../interfaces/UserLogin';
+import type { UserRegister } from '../interfaces/UserRegister';
 
 const login = async (userInfo: UserLogin) => {
     try {
@@ -23,4 +24,26 @@ const login = async (userInfo: UserLogin) => {
     }
 };
 
-export { login };
+const register = async (userInfo: UserRegister) => {
+    try {
+        const response = await fetch('/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userInfo),
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error('User information not retrieved, check network tab!');
+        }
+        return data;
+    } catch (err) {
+        console.log('Error from user register: ', err);
+        return Promise.reject('Could not fetch user info');
+    }
+};
+
+export { login, register };
