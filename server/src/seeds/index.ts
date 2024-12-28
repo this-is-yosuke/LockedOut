ec9d2adfab46d56d2ad0925574ec1127bbf79c6c
Kristy Thompson <kristy.h.thompson@gmail.com>
2024-12-28 16:41:42 -0700

    Added a query

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -7,6 +7,14 @@ const seedAll = async (): Promise<void> => {
         await sequelize.sync({force: true});
         console.log(`\n DATABASE SYNCED \n`);
 
+        await sequelize.query("ALTER TABLE attempt DROP CONSTRAINT IF EXISTS unique_user_room;");
+        await sequelize.query("ALTER TABLE attempt DROP CONSTRAINT IF EXISTS attempt_userId_key;");
+        await sequelize.query("ALTER TABLE attempt DROP CONSTRAINT IF EXISTS attempt_roomId_key;");
+        const results = await sequelize.query("SHOW COLUMNS FROM attempt;");
+        console.log('Columns:', results);
+
+        
+
         await seedDatabase();
         console.log(`\n Users seeded \n`);
 
