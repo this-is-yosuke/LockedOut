46a9a8d8fe5115bff59c96ad04532613f9089256
this-is-yosuke <yosuke.kibe@gmail.com>
2024-12-29 18:19:19 -0700

    Updated CTAbutton
    The button had its href pointing to localHost. Now, it's a Link element routing to the registration component.

--- client/src/components/CTAbutton.tsx
+++ client/src/components/CTAbutton.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
+
 const CTAButton: React.FC = () => {
     return (
-      <a
-        href="http://localhost:3001/register"
-        className="bg-red-600 hover:bg-red-700 text-white text-lg px-8 py-3 rounded-md inline-block transition duration-300"
-      >
-        PLAY NOW
-      </a>
+      <Link
+        to='/register'
+        className='bg-red-600 hover:bg-red-700 text-white text-lg px-8 py-3 rounded-md inline-block transition duration-300'>
+          PLAY NOW
+        </Link>
     );
   };
   
